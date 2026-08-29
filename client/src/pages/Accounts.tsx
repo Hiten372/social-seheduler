de6e5ca3e51377b2faebb1dd6bd0a4cf595483e3
hiten <hitenprasad2017jsr@gmail.com>
2026-08-30 00:29:48 +0530

    Refactor layout and sidebar components; streamline imports and enhance navigation structure

--- client/src/pages/Accounts.tsx
+++ client/src/pages/Accounts.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-
- const Accounts = () => {
-    return (
-        <div>
-            <h1>Accounts</h1>
-            <p>Welcome to the accounts page!</p>
-        </div>
-    );
+const Accounts = () => {
+  return (
+    <div>
+      <h1>Accounts</h1>
+      <p>Welcome to the accounts page!</p>
+    </div>
+  );
 };
 
 export default Accounts;
